b1c6e84aef9301169c13204ae0a4d0f43f03d938
jdegenstein <jdegenstein@users.noreply.github.com>
2024-01-08 15:49:28 -0600

    Create OCC_bottle.py

---
+++ examples/OCC_bottle.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('..')
+from __ocp_action_api import show_object
+from build123d import *
+
+#same as https://build123d.readthedocs.io/en/latest/introductory_examples.html#the-classic-occ-bottle
+
+L, w, t, b, h, n = 60.0, 18.0, 9.0, 0.9, 90.0, 6.0
+
+with BuildPart() as ex29:
+    with BuildSketch(Plane.XY.offset(-b)) as ex29_ow_sk:
+        with BuildLine() as ex29_ow_ln:
+            l1 = Line((0, 0), (0, w / 2))
+            l2 = ThreePointArc(l1 @ 1, (L / 2.0, w / 2.0 + t), (L, w / 2.0))
+            l3 = Line(l2 @ 1, Vector((l2 @ 1).X, 0, 0))
+            mirror(ex29_ow_ln.line)
+        make_face()
+    extrude(amount=h + b)
+    fillet(ex29.edges(), radius=w / 6)
+    with BuildSketch(ex29.faces().sort_by(Axis.Z)[-1]):
+        Circle(t)
+    extrude(amount=n)
+    necktopf = ex29.faces().sort_by(Axis.Z)[-1]
+    offset(ex29.solids()[0], amount=-b, openings=necktopf)
+
+show_object(ex29.part)
